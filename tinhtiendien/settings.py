e7416296b85bf6fc7a3c22a74ca3526ae8e70ff2
Unknown <rucchuvan.py@gmail.com>
2018-07-22 11:40:04 +0700

    add MEDIA setting

--- tinhtiendien/settings.py
+++ tinhtiendien/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -125,6 +126,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 try:
     from .local_settings import *
 except ImportError:
